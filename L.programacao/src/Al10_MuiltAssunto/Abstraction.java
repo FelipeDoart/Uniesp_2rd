9f5266c08c755b0489a8116caceed5a60841eed7
Felipe_Doart <flpd.araujo@outlook.com>
2025-10-27 21:04:00 -0300

    Add_Aula10

---
+++ L.programacao/src/Al10_MuiltAssunto/Abstraction.java
@@ -0,0 +1,26 @@
+/*
+ABSTRACTION:
+ - Em Java, o abstract é usado para criar uma classe que não pode ser instanciada diretamente,
+servindo como um modelo para outras classes. Classes abstratas podem conter tanto métodos
+abstratos.
+
+ - Para criar uma classe abstrata, usa-se a palavra-chave "abstract" antes da sua declaração.
+
+
+Classe abstrata
+> É uma classe que não pode ser instanciada;
+> Serve como uma classe base (superclasse) para outras classes;
+> Pode conter métodos abstratos, métodos concretos, variáveis e construtores;
+> Uma classe que contém pelo menos um metodo abstrato deve ser declarado como abstrata.
+
+Método abstrato
+> É um método sem implementação, declarado com a palavra-chave abstract e terminado por
+> um ponto e vírgula, em vez de chaves {}.
+> Obrigam as subclasses não abstratas a fornecerem uma implementação para esses métodos.
+> São usados para definir um "contrato" que as subclasses devem cumpri
+
+OVERRIDES
+ - Ele indica uma sobrescrevimento de um código feito anteriormente, sendo usado para atender
+as necessidades da classe atual
+
+*/
